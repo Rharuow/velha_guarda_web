519a0081e9f2a03da82473205dcd53626cd1851d
Harysson Soares <haryssonsoares@gmail.com>
2022-02-06 16:33:09 -0300

    feat: char component

--- components/Cards/Char.tsx
+++ components/Cards/Char.tsx
@@ -5,15 +5,22 @@ export type PropsChar = {
   name: string;
   sex: "m" | "f";
   voc: "ms" | "ek" | "ed" | "rp";
+  lvl: number;
 };
 
-const Char: React.FC<PropsChar> = ({ name, sex, voc }) => {
+const Char: React.FC<PropsChar> = ({ name, sex, voc, lvl }) => {
   return (
     <Card>
       <Card.Header className="d-flex align-items-center justify-content-between">
         <img src={`./${voc}${sex}.png`} alt="char icon" />
         <p className="text-center flex-grow-1">{name}</p>
       </Card.Header>
+      <Card.Body>
+        <div className="d-flex justify-content-between bg-secondary rounded px-2">
+          <span className="text-white fw-bold">Level:</span>
+          <strong>{lvl}</strong>
+        </div>
+      </Card.Body>
     </Card>
   );
 };
