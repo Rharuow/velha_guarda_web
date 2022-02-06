d6469a845dd178391f9872607a3902db8841271a
Harysson Soares <haryssonsoares@gmail.com>
2022-02-06 16:36:23 -0300

    feat: events slick cards

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -7,12 +7,18 @@ import Char from "../../components/Cards/Char";
 import Header from "../../components/domain/Header";
 import { useCurrentUserContext } from "../../components/Page/Application";
 import { CharDatabase } from "../../types/database/Char";
+import Event from "../../components/Cards/Event";
+import New from "../../components/Cards/New";
+import { EventDatabase } from "../../types/database/Event";
+import { getCharEvents } from "../../services/api";
+import { getSession } from "next-auth/react";
 
 const Home: React.FC = () => {
   const currentUser = useCurrentUserContext();
   const [loading, setLoading] = useState(true);
   const [chars, setChars] = useState<Array<CharDatabase>>();
   const [char, setChar] = useState<CharDatabase>();
+  const [events, setEvents] = useState<Array<EventDatabase>>();
 
   const settings = {
     speed: 500,
@@ -22,13 +28,24 @@ const Home: React.FC = () => {
     variableWidth: false,
   };
 
+  console.log("currentUser = ", currentUser);
+
   useEffect(() => {
+    const charEvents = async (id: string) => {
+      const session = await getSession();
+      let eventsTemp;
+      if (session) eventsTemp = await getCharEvents(id, session.token);
+      console.log(eventsTemp?.data.record.events);
+      setEvents(eventsTemp?.data.record.events);
+    };
     if (currentUser && currentUser !== null && currentUser.chars) {
       setChars(currentUser.chars.filter((c) => c.name !== char?.name));
       setChar(currentUser.chars[0]);
+      charEvents(currentUser.chars[0].id);
+
       setLoading(false);
     }
-  }, [currentUser]);
+  }, [char?.name, currentUser]);
 
   return (
     <div className="d-flex justify-content-center flex-wrap overflow-hidden">
@@ -39,6 +56,22 @@ const Home: React.FC = () => {
           </div>
           <div className="mb-4">
             <CharDetailed {...char} />
+          </div>
+          <div className="mb-4 w-100">
+            <Slider {...settings}>
+              {currentUser?.is_admin && (
+                <div key={char.name} className="px-2 ">
+                  <New title="Evento" />
+                </div>
+              )}
+              {events &&
+                events.length > 0 &&
+                events.map((ev) => (
+                  <div key={ev.name} className="px-2 ">
+                    <Event {...ev} />
+                  </div>
+                ))}
+            </Slider>
           </div>
           <div className="mb-4 w-100">
             <Slider {...settings} className="h-100">
